fac57849527aad2b03440ce657f186623ac8de23
Ricardo Dias <contato@ricardopedias.com.br>
2026-04-11 16:17:49 -0300

    Ajustes no TwigEngine

--- tests/TwigEngineTest.php
+++ tests/TwigEngineTest.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Tests;
 
 use Iquety\Presentation\Engine\Twig\TwigEngine;
-use Iquety\Presentation\Engine\ViewPathException;
+use Iquety\Presentation\Engine\PathException;
 
 class TwigEngineTest extends TestCase
 {
     /** @test */
     public function renderException(): void
     {
-        $this->expectException(ViewPathException::class);
+        $this->expectException(PathException::class);
         $this->expectExceptionMessage('No view path was added.');
 
         $engine = new TwigEngine();
@@ -27,8 +27,8 @@ class TwigEngineTest extends TestCase
         $engine->addViewPath(__DIR__.'/Stubs/TwigOne');
         $engine->addViewPath(__DIR__.'/Stubs/TwigTwo');
 
-        $this->assertSame('Hello Ricardo!', $engine->render('hello.html', ['name' => 'Ricardo']));
-        $this->assertSame('Bye Ricardo!', $engine->render('bye.html', ['name' => 'Ricardo']));
+        $this->assertSame('Hello, Ricardo!', $engine->render('hello', ['name' => 'Ricardo']));
+        $this->assertSame('Bye, Ricardo!', $engine->render('bye', ['name' => 'Ricardo']));
     }
 }
 
