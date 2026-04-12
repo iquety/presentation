a7742afb41f8cbb94edc0df1ba335d21e6c729bb
Ricardo Dias <contato@ricardopedias.com.br>
2026-04-12 10:57:54 -0300

    Testes de unidade para o adaptador LatteEngine

---
+++ tests/LatteEngineTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Iquety\Presentation\Engine\Latte\LatteEngine;
+use Iquety\Presentation\Engine\PathException;
+
+class LatteEngineTest extends TestCase
+{
+    /** @test */
+    public function renderViewPathException(): void
+    {
+        $this->expectException(PathException::class);
+        $this->expectExceptionMessage('No view path was added.');
+
+        $engine = new LatteEngine();
+
+        $engine->render('hello', []);
+    }
+
+    /** @test */
+    public function render(): void
+    {
+        $engine = new LatteEngine();
+        $engine->addViewPath(__DIR__.'/Stubs/LatteOne');
+        $engine->addViewPath(__DIR__.'/Stubs/LatteTwo');
+        $engine->setCachePath(__DIR__.'/Stubs/LatteCache');        
+
+        $this->assertSame('Hello, Ricardo!', $engine->render('folder.hello', ['name' => 'Ricardo']));
+        $this->assertSame('Bye, Ricardo!', $engine->render('folder.bye', ['name' => 'Ricardo']));
+    }
+}
+
